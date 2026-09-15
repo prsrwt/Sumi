605dd7bc5e7933a39f5f221f3fa3c3d7732abb70
prsrwt <rwt.prs2005@gmail.com>
2026-09-15 10:42:43 +0530

    Rename the app ID to io.github.prsrwt.sumi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.sumi.app"
+        applicationId = "io.github.prsrwt.sumi"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
